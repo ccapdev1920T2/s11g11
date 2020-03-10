dc6df7253b86858040dbd3e0665980d6b0bfd23b
james <james@LAPTOP-G2IVGM25>
2020-03-10 16:55:00 +0800

    Merge origin/master
    Conflicts:
    	nbproject/project.xml
    	package.json

--- router/animoRouter.js
+++ router/animoRouter.js
@@ -7,5 +7,15 @@ const middleware = require('../middlewares/animoMiddleware');
 const controller = require('../controller/index');
 
 router.get('/', controller.getHome);
+router.get('/home', controller.getHome);
+router.get('/login', controller.getLogin);
+router.get('/register', controller.getRegister);
+router.get('/profile', controller.getProfile);
+router.get('/viewcourses', controller.getCourseOffer);
+router.get('/addclass', controller.getAddClass);
+router.get('/dropclass', controller.getDropClass);
+router.get('/swapclass', controller.getSwapClass);
 
+router.post('/login', middleware.validateLogin, controller.postLogin);
+router.post('/register', middleware.validateRegister, controller.postRegister);
 modules.exports = animoRouter;
