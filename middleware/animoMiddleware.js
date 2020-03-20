c15fc0b8a2cf1e9562fb94e68f9338410901d872
Maku <Maku@LAPTOP-147I866T>
2020-03-20 23:27:55 +0800

    merge kressha & dwynit branches to master

--- middleware/animoMiddleware.js
+++ middleware/animoMiddleware.js
@@ -1,6 +1,42 @@
+
+function validateEmail(email){
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email);
+}
+
 const animoMiddleware = {
+    validateLogin : function (req, res, next){
+        const {email, password} = req.body;
+
+        if(!email){
+            return res.status(401).end('401 Unathorized error, no email');
+        }
+        else if(!validateEmail(email)){
+            return res.status(401).end('401 Unathorized error, invalid email');
+        }
+        if(!password){
+            return res.status(401).end('401 Unathorized error, no password');
+        }
+    
+    next();
+    
+    },
+    
+    validateRegister : function (req, res, next){
+         const {idNum, email, fName, lName, college, degree, password, conPass} = req.body;
+         
+        if(!idNum || !email || !fName || !lName || !college || !degree || !password || !conPass){
+            return res.status(401).end('401 Unathorized error, missing input');
+         }
+        else if(!validateEmail(email)){
+            return res.status(401).end('401 Unathorized error, invalid email');
+        }
+        else if(password !== conPass){
+            return res.status(401).end('401 Unathorized error, passwords do not match');
+        }
+        else next();        
+    }
     
 };
 
 module.exports = animoMiddleware;
-
