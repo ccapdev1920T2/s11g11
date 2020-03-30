666f3a7ec935285d5e2d37b113128b7b42bb50f2
james <james@LAPTOP-G2IVGM25>
2020-03-31 01:46:08 +0800

    Complete working middlewares, fixed addclass and swapclass handlebars

--- middleware/animoMiddleware.js
+++ middleware/animoMiddleware.js
@@ -57,11 +57,11 @@ const animoMiddleware = {
 			return res.status(401).end('401 Unauthorized error, missing input');
 
 		else{
-			var classNumber = await classModel.findOne({classNum: searchAddC});
+			let classNumber = await classModel.findOne({classNum: searchAddC});
 			if (classNumber === null)
 				return res.status(401).end('401 Unauthorized error, class number does not exist');
 
-			var studClass = await studentModel.findOne({email: req.session.user.email}).populate('classList');
+			let studClass = await studentModel.findOne({email: req.session.user.email}).populate('classList');
 			
 			// get the student match, convert BSON to JSON, then store the classList to a variable
 			let classes = JSON.parse(JSON.stringify(studClass)).classList; // classList and classes are arrays
@@ -79,78 +79,77 @@ const animoMiddleware = {
 		} 
 	},
 
-	validateDropClass: function (req, res, next) {
+	validateDropClass: async function (req, res, next) {
 		let {searchDropC} = req.body;
 
 		if(!searchDropC)
 			return res.status(401).end('401 Unauthorized error, missing input');
 
 		else{
-			studentModel.findOne({email: req.session.user.email})
-				.populate('classList')
-				.then(function(match) {
-				// get the student match, convert BSON to JSON, then store the classList to a variable
-				let classes = JSON.parse(JSON.stringify(match)).classList; // classList and classes are arrays
+			let classNumber = await classModel.findOne({classNum: searchDropC});
+			if (classNumber === null)
+				return res.status(401).end('401 Unauthorized error, class number does not exist');
 
-				// get an array that contains class->classNum that match the searchDropC
-				let classMatch = classes.filter(function(elem) {
-					return elem.classNum === searchDropC;
-				});
+			let studClass = await studentModel.findOne({email: req.session.user.email}).populate('classList');
+			
+			// get the student match, convert BSON to JSON, then store the classList to a variable
+			let classes = JSON.parse(JSON.stringify(studClass)).classList; // classList and classes are arrays
 
-				// if classMatch is empty, that means that the class does not exist in student's class list
-				if (classMatch.length === 0) {
-					return res.status(401).end('401 Unauthorized error, class does not exist in class list');
-				}
+			// get an array that contains class->classNum that match the searchDropC
+			let classMatch = classes.filter(function(elem) {
+				return elem.classNum === searchDropC;
 			});
+
+			// if classMatch is empty, that means that the class does not exist in student's class list
+			if (classMatch.length === 0) {
+				return res.status(401).end('401 Unauthorized error, class does not exist in class list');
+			}
+			else return next();
 		}
-		next();
 	},
 	
-	validateSwapClass: function (req, res, next) {
+	validateSwapClass: async function (req, res, next) {
 		let {add, drop} = req.body;
 
 		if(!drop || !add)
 			return res.status(401).end('401 Unauthorized error, missing input');
 
 		else{
-			classModel.findOne({classNum: add}, function(err, match) {
-				if(err)
-					return res.status(500).end('500 Server error, cannot connect to db');
-				else if (!match)
-					return res.status(401).end('401 Unauthorized error, class number does not exist');
+			let aClassNumber = await classModel.findOne({classNum: add});
+			if (aClassNumber === null)
+				return res.status(401).end('401 Unauthorized error, class number you want to add does not exist');
+			
+			let dClassNumber = await classModel.findOne({classNum: drop});
+			if (dClassNumber === null)
+				return res.status(401).end('401 Unauthorized error, class number you want to drop does not exist');
+
+			let studClass = await studentModel.findOne({email: req.session.user.email}).populate('classList');
+			
+			// get the student match, convert BSON to JSON, then store the classList to a variable
+			let classes = JSON.parse(JSON.stringify(studClass)).classList; // classList and classes are arrays
+
+			// get an array that contains class->classNum that match the add
+			let addMatch = classes.filter(function(elem) {
+				return elem.classNum === add;
 			});
-			studentModel.findOne({email: req.session.user.email})
-					.populate('classList')
-					.then(function(match) {
-				// get the student match, convert BSON to JSON, then store the classList to a variable
-				let classes = JSON.parse(JSON.stringify(match)).classList; // classList and classes are arrays
-
-				{
-					// get an array that contains class->classNum that match the add
-					let classMatch = classes.filter(function(elem) {
-						return elem.classNum === add;
-					});
-
-					// if classMatch is NOT empty, that means that the class already exists in student's class list
-					if (classMatch.length > 0) {
-						return res.status(401).end('401 Unauthorized error, class already exists in class list');
-					}
-				}
-
-				{
-					// get an array that contains class->classNum that match the drop
-					let classMatch = classes.filter(function(elem) {
-						return elem.classNum === drop;
-					});
-
-					// if classMatch is empty, that means that the class does not exist in student's class list
-					if (classMatch.length === 0) {
-						return res.status(401).end('401 Unauthorized error, class does not exist in class list');
-					}
-				}
+			
+			// get an array that contains class->classNum that match the drop
+			let dropMatch = classes.filter(function(elem) {
+				return elem.classNum === drop;
 			});
+
+			// if addMatch is NOT empty, that means that the class already exists in student's class list
+			if (addMatch.length > 0) {
+				return res.status(401).end('401 Unauthorized error, class to add already exists in class list');
+			}
+			
+			// if dropMatch is empty, that means that the class does not exist in student's class list
+			else if (dropMatch.length === 0) {
+				return res.status(401).end('401 Unauthorized error, class to drop does not exist in class list');
+			}
+			
+			else return next();
 		}
-		next();
 	}
 
 };
